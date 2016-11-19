ea9ba93618fe6745032c96f9ee2f0c89cd3da734
Jacob Bom <bomjacob@gmail.com>
2016-11-19 12:20:19 +0100

    Add static url to env

--- htxaarhuslan/settings.py
+++ htxaarhuslan/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = env_var('STATIC_URL', '/static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
