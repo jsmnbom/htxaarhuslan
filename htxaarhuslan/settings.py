fe25012b56e3d910f776777dae935de75ec6c1e1
Jacob Bom <bomjacob@gmail.com>
2016-11-19 12:23:25 +0100

    Properly do static stuff

--- htxaarhuslan/settings.py
+++ htxaarhuslan/settings.py
@@ -132,7 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = env_var('STATIC_URL', '/static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    env_var('STATIC_DIR', '')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
