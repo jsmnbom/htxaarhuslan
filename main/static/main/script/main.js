617a5d41d5e832d8bc99d4becfbe98777fc4e596
Jacob Bom <bomjacob@gmail.com>
2016-11-29 20:06:27 +0100

    Reformat and clean up JS

--- main/static/main/script/main.js
+++ main/static/main/script/main.js
@@ -1,118 +1,127 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(".header .title").fitText(1.9);
     $(".header .nav a").fitText(0.7);
 
     // Cookie law stuff
     window.cookieconsent.initialise({
-      "palette": {
-        "popup": {
-          "background": "#363032",
-          "text": "#ffffff"
+        "palette": {
+            "popup": {
+                "background": "#363032",
+                "text": "#ffffff"
+            },
+            "button": {
+                "background": "#d33e20",
+                "text": "#ffffff"
+            }
         },
-        "button": {
-          "background": "#d33e20",
-          "text": "#ffffff"
+        "theme": "edgeless",
+        "position": "bottom-left",
+        "content": {
+            "message": "Denne hjemmeside bruger cookies til at sikre, at du får den bedste oplevelse på vores hjemmeside.",
+            "dismiss": "Forstået!",
+            "link": "Lær mere",
+            "href": "/privatliv"
         }
-      },
-      "theme": "edgeless",
-      "position": "bottom-left",
-      "content": {
-        "message": "Denne hjemmeside bruger cookies til at sikre, at du får den bedste oplevelse på vores hjemmeside.",
-        "dismiss": "Forstået!",
-        "link": "Lær mere",
-        "href": "/privatliv"
-      }
     });
 
     // Hide edit profile until you click button
     var hidden = false;
-    $('.box#profile .hide').hide()
+    $('.box#profile .hide').hide();
     if ($('.box#profile #edit.hide').length) {
         hidden = true
     }
-    $('.box#profile form input[type="submit"]').click(function(e) {
+    $('.box#profile form input[type="submit"]').click(function (e) {
         if (hidden) {
-            e.preventDefault()
-            $('.box#profile #show').slideUp()
-            $('.box#profile #edit').slideDown()
+            e.preventDefault();
+            $('.box#profile #show').slideUp();
+            $('.box#profile #edit').slideDown();
             hidden = false
         }
-        setTimeout(function() { $(window).resize() }, 500);
+        setTimeout(function () {
+            $(window).resize()
+        }, 500);
     });
 
     // Proper file upload (make it so you can see which file you've selected, even if we do the fancy label thing we did
     var input = $('div.file input[type="file"]'),
         label = $('div.file .choose'),
         labelText = label.html();
-    input.on('change', function(e) {
-        var fileName = e.target.value.split( '\\' ).pop();
+    input.on('change', function (e) {
+        var fileName = e.target.value.split('\\').pop();
 
-        if (fileName)
-            label.html(fileName)
-        else
-            label.html(labelText)
-    })
+        if (fileName) {
+            label.html(fileName);
+        } else {
+            label.html(labelText);
+        }
+    });
 
 
     // Footer spacer
-    $(window).resize(function() {
-        $('.spacer').height($('.sidebar').height()-$('.page').height()+$('.spacer').height()+$(window).height()*0.05)
+    $(window).resize(function () {
+        var spacer = $('.spacer');
+        spacer.height($('.sidebar').height() - $('.page').height() + spacer.height() + $(window).height() * 0.05)
     });
-    setTimeout(function() { $(window).resize() }, 500);
+    setTimeout(function () {
+        $(window).resize()
+    }, 500);
 
     // Show input helptext only when input is focussed
-    $('form span.helptext').each(function() {
-        var input = $(this).prev().prev()
-        var helpText = $(this)
-        input.focus(function() {
+    $('form span.helptext').each(function () {
+        var input = $(this).prev().prev();
+        var helpText = $(this);
+        input.focus(function () {
             helpText.slideDown()
-        }).blur(function() {
+        }).blur(function () {
             helpText.slideUp()
         });
     });
 
     /* Countdown clock (if tilmelding isn't open yet) */
     if ($('h3#notopen').length) {
-        var remainingTimeMillis = parseInt($('div#countdown').attr('data-opens')) * 1000
+        var div = $('div#countdown');
+        var remainingTimeMillis = parseInt(div.attr('data-opens')) * 1000;
         var now = new Date();
-        var opens = new Date(now.getTime() + remainingTimeMillis)
-        console.log(remainingTimeMillis, now, opens)
-        $('div#countdown').countdown(opens, function(event) {
+        var opens = new Date(now.getTime() + remainingTimeMillis);
+        console.log(remainingTimeMillis, now, opens);
+        div.countdown(opens, function (event) {
             if (event.offset.totalDays >= 1) {
                 $(this).html(event.strftime('%-D %!D:dag,dage;, %H:%M:%S'));
             } else {
                 $(this).html(event.strftime('%H:%M:%S'));
             }
         });
-        setTimeout(function() { $('div#countdown').fitText(0.6) }, 500);
+        setTimeout(function () {
+            $('div#countdown').fitText(0.6)
+        }, 500);
     }
 
     /* Pretty tilmeldlist table */
-    var table = $('#tilmeldlist table')
+    var table = $('#tilmeldlist').find('table');
     if (table.length) {
         table.DataTable({
             paging: false,
             responsive: {
-            details: {
-                display: $.fn.dataTable.Responsive.display.childRowImmediate,
-                type: ''
-            }
-        },
+                details: {
+                    display: $.fn.dataTable.Responsive.display.childRowImmediate,
+                    type: ''
+                }
+            },
             language: {
-                "sProcessing":   "Henter...",
-                "sLengthMenu":   "Vis _MENU_ tilmeldte",
-                "sZeroRecords":  "Ingen tilmeldte matcher s&oslash;gningen",
-                "sInfo":         "Viser _START_ til _END_ af _TOTAL_ tilmeldte",
-                "sInfoEmpty":    "Viser 0 til 0 af 0 tilmeldte",
+                "sProcessing": "Henter...",
+                "sLengthMenu": "Vis _MENU_ tilmeldte",
+                "sZeroRecords": "Ingen tilmeldte matcher s&oslash;gningen",
+                "sInfo": "Viser _START_ til _END_ af _TOTAL_ tilmeldte",
+                "sInfoEmpty": "Viser 0 til 0 af 0 tilmeldte",
                 "sInfoFiltered": "(filtreret fra _MAX_ tilmeldte)",
-                "sInfoPostFix":  "",
-                "sSearch":       "S&oslash;g:",
-                "sUrl":          "",
+                "sInfoPostFix": "",
+                "sSearch": "S&oslash;g:",
+                "sUrl": "",
                 "oPaginate": {
-                    "sFirst":    "F&oslash;rste",
+                    "sFirst": "F&oslash;rste",
                     "sPrevious": "Forrige",
-                    "sNext":     "N&aelig;ste",
-                    "sLast":     "Sidste"
+                    "sNext": "N&aelig;ste",
+                    "sLast": "Sidste"
                 }
             }
         })
@@ -124,13 +133,13 @@ $(document).ready(function() {
 (function () {
     var div = document.createElement('div'),
         ref = document.getElementsByTagName('base')[0] ||
-              document.getElementsByTagName('script')[0];
+            document.getElementsByTagName('script')[0];
 
     div.innerHTML = '&shy;<style> iframe { visibility: hidden; } </style>';
 
     ref.parentNode.insertBefore(div, ref);
 
-    window.onload = function() {
+    window.onload = function () {
         div.parentNode.removeChild(div);
     }
 })();
--- main/static/main/script/tilmeld.js
+++ main/static/main/script/tilmeld.js
@@ -1,23 +1,24 @@
-$(document).ready(function() {
-    $(window).resize(function() {
-        var m = -1
-        $('.seats table td').each(function() {
-            var h = $(this).width()
+$(document).ready(function () {
+    var tabletd = $('.seats table td');
+    $(window).resize(function () {
+        var m = -1;
+        tabletd.each(function () {
+            var h = $(this).width();
             if (h > m) {
                 m = h
             }
-        })
-        var s = m/3
+        });
+        var s = m / 3;
         $('.seats table td').css({height: m, fontSize: s})
     }, 200).resize();
 
-    $('.seats table td').each(function() {
+    tabletd.each(function () {
         if ($(this).attr('data-name') !== undefined) {
             if ($(this).attr('data-name') !== '') {
-                $(this).addClass('occupied')
-                var url = $(this).attr('data-url')
-                var name = $(this).attr('data-name')
-                var grade = $(this).attr('data-grade')
+                $(this).addClass('occupied');
+                var url = $(this).attr('data-url');
+                var name = $(this).attr('data-name');
+                var grade = $(this).attr('data-grade');
                 $(this).qtip({
                     content: {
                         text: '<a href="' + url + '">' + name + '</a>' + '<br>' + '<span>' + grade + '</span>'
@@ -25,16 +26,16 @@ $(document).ready(function() {
                     position: {
                         my: "left center",
                         at: 'right center',
-                        viewport: true,
+                        viewport: true
                     },
                     style: {
-                        classes: "qtip-seat",
+                        classes: "qtip-seat"
                     },
                     events: {
-                        visible: function(event, api) {
+                        visible: function (event, api) {
                             $(event.originalEvent.target).addClass('open')
                         },
-                        hide: function(event, api) {
+                        hide: function (event, api) {
                             $(event.originalEvent.target).removeClass('open')
                         }
                     }
@@ -53,42 +54,42 @@ $(document).ready(function() {
         }
     }
 
-    var seat = $('.seats h4#current').attr('data-seat')
+    var seat = $('.seats h4#current').attr('data-seat');
 
     if (seat != undefined) {
-        $('input[name="seat"]').val(seat)
+        $('input[name="seat"]').val(seat);
         $('.seats form#tilmeld').hide()
     }
 
-    $('.seats table td').click(function() {
+    tabletd.click(function () {
         if ($(this).hasClass('selected')) {
-            $('.seats td').removeClass('selected')
+            $('.seats td').removeClass('selected');
             if (seat) {
                 $('input[name="seat"]').val(seat)
             } else {
                 $('input[name="seat"]').val('')
             }
         } else {
-            $('.seats td').removeClass('selected')
+            $('.seats td').removeClass('selected');
             if ($(this).hasClass('availible')) {
-                $(this).addClass('selected')
+                $(this).addClass('selected');
                 $('input[name="seat"]').val($(this).text())
             } else if ($(this).hasClass('current')) {
                 $('input[name="seat"]').val($(this).text())
             } else {
-                $('input[name="seat"]').val(seat)
+                $('input[name="seat"]').val(seat);
                 if ($(this).hasClass('open')) {
-                    window.location.href=$(this).attr('data-url');
+                    window.location.href = $(this).attr('data-url');
                 }
             }
         }
         showhideform()
     });
 
-    $('.seats a.button#reset').click(function(e) {
-        e.preventDefault()
-        $('.seats table td').removeClass('selected')
-        $('input[name="seat"]').val('')
+    $('.seats a.button#reset').click(function (e) {
+        e.preventDefault();
+        $('.seats table td').removeClass('selected');
+        $('input[name="seat"]').val('');
         showhideform()
     });
 });
